b8a4e9f7340601d38e8173f21f904571dbbf8c2d
Shakira Perry <shaaperr@ut.utm.edu>
2023-03-02 14:03:37 -0600

    Add project files.

---
+++ CW9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW9
+{
+   abstract class FindPiThread
+    {
+        int darts;
+        int count;
+        int random;
+    }
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
